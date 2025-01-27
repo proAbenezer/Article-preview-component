0334aeff79be3f20e697dc74dfb955a3b72ddeb9
Abenezer Haile <devabenezer7@gmail.com>
2025-01-27 12:30:39 +0300

    finsihed the mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "serif"],
+      },
+      letterSpacing: {
+        oneRem: ".5rem",
+      },
+      colors: {
+        VeryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        DesaturatedDarkBlue: "hsl(214, 17%, 51%)",
+        GrayishBlue: "hsl(212, 23%, 69%)",
+        LightGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
