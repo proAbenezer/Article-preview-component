09d8148a9358cd8d8b07e277e3d0f2ed5aa00385
Abenezer Haile <devabenezer7@gmail.com>
2025-01-29 20:59:04 +0300

    fixed the arrow for the social links on large screen size

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const socialLinksBtn1 = document.getElementById("social-links-btn1");
 const socialLinksBtn2 = document.getElementById("social-links-btn2");
 const sociaLinks = document.getElementById("social-links");
-const afterElement = document.getElementById("afterElement");
 socialLinksBtn1.addEventListener("click", () => {
   if (sociaLinks.style.display === "none") {
     sociaLinks.style.display = "flex"; // Set to flex if it was hidden
-    afterElement.style.display = "block";
   } else {
     sociaLinks.style.display = "none"; // Hide it by setting display to none
-    afterElement.style.display = "none";
   }
 });
 socialLinksBtn2.addEventListener("click", () => {
